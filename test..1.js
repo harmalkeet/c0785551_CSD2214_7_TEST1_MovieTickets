c8085e843ddfe325a55df79e196509666bd42ba4
harmalkeet <hhundal446@gmail.com>
2021-03-12 07:33:11 +0530

    add css and js

---
+++ test..1.js
@@ -0,0 +1,33 @@
+
+fuction onloaderFunc()
+{
+    $(".seatStructu/+0re*").prop("disabled", true);
+    $("displayBoxes*").prop("disabled", true);
+
+
+}
+fuction takeData()
+{
+    if (( $("#username").val().length== 0 ) ||(
+        $("#Numbers").val().length == 0 ))
+        {
+            alert("enter name and number of the seat");
+
+        }
+        else
+        {
+            $(".inputForm*").prop("disabled",true);
+            $(".seatStructure*").prop("disabled",false);
+            document.getElementById("notification").innerHTML="<b"
+            style='margin-bottom:0px;background:yellow;'>"Please Select your Seats Now!</b>";
+        }
+    )
+}
+function updateTextArea() {
+    if ($("input:checked").length==($("Numseats").val()))
+    {
+        $(".seatStructure*").prop("disabled", true);
+        var allN
+    }
+
+}
